192923f6d87348797f82d1f588d1407827056bfe
Jen Weber <j@jenweber.me>
2022-03-16 23:30:40 -0400

    Story data is filled in

--- app/services/form-data.js
+++ app/services/form-data.js
@@ -1,3 +1,16 @@
 import Service from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
-export default class FormDataService extends Service {}
+export default class FormDataService extends Service {
+  @tracked name = '';
+  @tracked soundA = '';
+  @tracked vehicle = '';
+  @tracked animal = '';
+  @tracked soundB = '';
+  @tracked nonsenseA = '';
+  @tracked nonsenseB = '';
+  @tracked verb = '';
+  @tracked noun = '';
+  @tracked nonsenseC = '';
+  @tracked nonsenseD = '';
+}
