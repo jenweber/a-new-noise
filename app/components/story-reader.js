756c4f8c96be83ea38d3efab83b5a324cbb735b3
Jen Weber <j@jenweber.me>
2022-03-19 16:05:39 -0400

    Speech to text works

--- app/components/story-reader.js
+++ app/components/story-reader.js
@@ -1,6 +1,32 @@
 import Component from '@glimmer/component';
 import { service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class StoryReaderComponent extends Component {
   @service formData;
+
+  @action play() {
+    // https://stackoverflow.com/questions/21513706/getting-the-list-of-voices-in-speechsynthesis-web-speech-api
+    async function speak() {
+      await initVoices();
+      const text = document.querySelector('#story').innerText;
+      const msg = new SpeechSynthesisUtterance(text);
+      // 1, 10, 11, 33
+      msg.voice = speechSynthesis.getVoices()[50];
+      msg.rate = 0.9;
+      speechSynthesis.speak(msg);
+    }
+
+    function initVoices() {
+      return new Promise(function (res, rej) {
+        speechSynthesis.getVoices();
+        if (window.speechSynthesis.onvoiceschanged) {
+          res();
+        } else {
+          window.speechSynthesis.onvoiceschanged = () => res();
+        }
+      });
+    }
+    speak();
+  }
 }
