6aefd52e8770b5bae19c2b3ef081a14cbab20075
Jen Weber <j@jenweber.me>
2022-03-22 22:02:16 -0400

    Move narrator stuff to service

--- app/components/story-reader.js
+++ app/components/story-reader.js
@@ -1,54 +1,12 @@
 import Component from '@glimmer/component';
 import { service } from '@ember/service';
 import { action } from '@ember/object';
-
-const key = '';
-
-// https://stackoverflow.com/questions/15653145/using-google-text-to-speech-in-javascript
-
-var textToSpeech = function (state) {
-  const url = `https://texttospeech.googleapis.com/v1/text:synthesize?key=${key}`;
-  const data = {
-    input: {
-      text: document.querySelector('#story').innerText,
-    },
-    voice: {
-      languageCode: 'en-us',
-      name: 'en-US-Standard-I', // en-US-Wavenet-I
-    },
-    audioConfig: {
-      audioEncoding: 'MP3',
-      pitch: 2,
-      speakingRate: 0.93,
-    },
-  };
-
-  const otherparam = {
-    headers: {
-      'content-type': 'application/json; charset=UTF-8',
-    },
-    body: JSON.stringify(data),
-    method: 'POST',
-  };
-  fetch(url, otherparam)
-    .then((data) => {
-      return data.json();
-    })
-    .then((res) => {
-      console.log(res.audioContent);
-      var audioElement = new Audio('data:audio/mp3;base64,' + res.audioContent);
-      audioElement.play();
-    })
-    .catch((error) => {
-      console.log(error);
-      state.onError(error);
-    });
-};
-
 export default class StoryReaderComponent extends Component {
   @service formData;
+  @service narrator;
 
   @action play() {
-    textToSpeech();
+    const text = document.querySelector('#story').innerText;
+    this.narrator.narrate(text);
   }
 }
--- app/services/narrator.js
+++ app/services/narrator.js
@@ -1,3 +1,58 @@
 import Service from '@ember/service';
 
-export default class NarratorService extends Service {}
+const key = '';
+
+// https://stackoverflow.com/questions/15653145/using-google-text-to-speech-in-javascript
+
+const fetchNarration = function (text) {
+  const url = `https://texttospeech.googleapis.com/v1/text:synthesize?key=${key}`;
+  const data = {
+    input: { text },
+    voice: {
+      languageCode: 'en-us',
+      name: 'en-US-Standard-I', // en-US-Wavenet-I
+    },
+    audioConfig: {
+      audioEncoding: 'MP3',
+      pitch: 2,
+      speakingRate: 0.93,
+    },
+  };
+
+  const opts = {
+    headers: {
+      'content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(data),
+    method: 'POST',
+  };
+
+  return fetch(url, opts)
+    .then((data) => {
+      return data.json();
+    })
+    .then((res) => {
+      var audioElement = new Audio('data:audio/mp3;base64,' + res.audioContent);
+      audioElement.play();
+      Promise.resolve(audioElement);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export default class NarratorService extends Service {
+  audioElement;
+
+  async narrate(text) {
+    this.audioElement = await fetchNarration(text);
+  }
+
+  pause() {
+    this.audioElement.pause();
+  }
+
+  resume() {
+    this.audioElement.resume();
+  }
+}
