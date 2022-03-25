7db85a1027eed1061538fdd72df80ec1a82ab4fa
Jen Weber <j@jenweber.me>
2022-03-24 23:41:44 -0400

    Simplify selfie component

--- app/components/selfie.js
+++ app/components/selfie.js
@@ -1,59 +1,16 @@
 import Component from '@glimmer/component';
-import { inject as service } from '@ember/service';
-
-const imageCapture = () => {
-  const video = document.getElementById('video');
-  const canvas = document.getElementById('canvas');
-  const snap = document.getElementById('snap');
-  let stream;
-
-  const constraints = {
-    audio: false,
-    video: {
-      width: 1280,
-      height: 720,
-    },
-  };
-
-  // Access webcam
-  async function init() {
-    try {
-      stream = await navigator.mediaDevices.getUserMedia(constraints);
-      handleSuccess(stream);
-    } catch (e) {
-      console.log(e.toString());
-    }
-  }
-
-  // Success
-  function handleSuccess(stream) {
-    window.stream = stream;
-    video.srcObject = stream;
-  }
-
-  // Load init
-  init();
-
-  // Draw image
-  var context = canvas.getContext('2d');
-  snap.addEventListener('click', function () {
-    context.drawImage(video, 0, 0, 400, 250);
-    const tracks = window.stream.getTracks();
-
-    tracks[0].stop();
-  });
-};
-
+import { service } from '@ember/service';
+import { action } from '@ember/object';
 export default class SelfieComponent extends Component {
   @service camera;
 
-  constructor() {
-    super(...arguments);
-    // this.camera;
-    // imageCapture();
+  @action
+  takeSnapshot() {
+    this.camera.savePhoto(document.querySelector('#canvas'));
   }
 
-  initCamera() {
-    imageCapture();
+  @action
+  turnOnCamera() {
+    this.camera.initCamera();
   }
 }
