94bcfb486cc83e6846531d5ef506a5d575cff3df
Jen Weber <j@jenweber.me>
2022-04-06 23:55:18 -0400

    Add murphy selfie

--- app/components/story-reader.js
+++ app/components/story-reader.js
@@ -18,5 +18,10 @@ export default class StoryReaderComponent extends Component {
   @action renderCanvas(canvasEl) {
     let context = canvasEl.getContext('2d');
     context.drawImage(this.camera.video, 0, 0, 400, 250);
+    let drawing = new Image();
+    drawing.src = 'murphy-drawing.png';
+    drawing.onload = function () {
+      context.drawImage(drawing, 270, 70, 162, 236);
+    };
   }
 }
